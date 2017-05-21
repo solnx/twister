c99681ba66d8dbdd91059ab5e0dc235241e5d4d5
Jörg Pernfuß <code.jpe@gmail.com>
2017-05-21 17:04:07 +0000

    Commit first draft of Twister

---
+++ lib/twister/twister.go
@@ -0,0 +1,159 @@
+package twister
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/mjolnir42/erebos"
+	"github.com/mjolnir42/legacy"
+	kazoo "github.com/wvanbergen/kazoo-go"
+)
+
+// Twister splits up read metric batches and produces the result
+type Twister struct {
+	Num      int
+	Input    chan []byte
+	Shutdown chan struct{}
+	Death    chan struct{}
+	Config   *erebos.Config
+	dispatch chan<- *sarama.ProducerMessage
+	producer sarama.AsyncProducer
+}
+
+// Start sets up the Twister application
+func (t *Twister) Start() {
+	kz, err := kazoo.NewKazooFromConnectionString(
+		t.Config.Zookeeper.Connect, nil)
+	if err != nil {
+		close(t.Death)
+		<-t.Shutdown
+		return
+	}
+	brokers, err := kz.BrokerList()
+	if err != nil {
+		kz.Close()
+		close(t.Death)
+		<-t.Shutdown
+		return
+	}
+	kz.Close()
+
+	host, err := os.Hostname()
+	if err != nil {
+		close(t.Death)
+		<-t.Shutdown
+		return
+	}
+
+	config := sarama.NewConfig()
+	config.Net.KeepAlive = 5 * time.Second
+	config.Producer.RequiredAcks = sarama.NoResponse
+	config.Producer.Retry.Max = 5
+	config.Producer.Partitioner = sarama.NewHashPartitioner
+	config.ClientID = fmt.Sprintf("twister.%s", host)
+
+	t.producer, err = sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		close(t.Death)
+		<-t.Shutdown
+		return
+	}
+	t.dispatch = t.producer.Input()
+	t.run()
+}
+
+// run is the event loop for Twister
+func (t *Twister) run() {
+	// required during shutdown
+	inputEmpty := false
+	errorEmpty := false
+	producerClosed := false
+
+runloop:
+	for {
+		select {
+		case <-t.Shutdown:
+			// received shutdown, drain input channel which will be
+			// closed by main
+			goto drainloop
+		case <-t.producer.Errors():
+			close(t.Death)
+			<-t.Shutdown
+			break runloop
+		case msg := <-t.Input:
+			if msg == nil {
+				// this can happen if we read the closed Input channel
+				// before the closed Shutdown channel
+				continue runloop
+			}
+			t.process(msg)
+		}
+	}
+	// shutdown due to producer error
+	producerClosed = true
+	t.producer.Close()
+	return
+
+drainloop:
+	for {
+		select {
+		case msg := <-t.Input:
+			if msg == nil {
+				// channel is closed
+				inputEmpty = true
+
+				if !producerClosed {
+					t.producer.Close()
+					producerClosed = true
+				}
+				if inputEmpty && errorEmpty {
+					break drainloop
+				}
+			}
+			t.process(msg)
+		case e := <-t.producer.Errors():
+			if e == nil {
+				errorEmpty = true
+
+				// channel is closed
+				if inputEmpty && errorEmpty {
+					break drainloop
+				}
+			}
+		}
+	}
+}
+
+// process is the handler for converting a MetricBatch
+// and producing the result
+func (t *Twister) process(msg []byte) {
+	batch := legacy.MetricBatch{}
+	if err := json.Unmarshal(msg, &batch); err != nil {
+		close(t.Death)
+		<-t.Shutdown
+		return
+	}
+
+	msgs := batch.Split()
+	for i := range msgs {
+		data, err := json.Marshal(&msgs[i])
+		if err != nil {
+			close(t.Death)
+			return
+		}
+
+		t.dispatch <- &sarama.ProducerMessage{
+			Topic: t.Config.Kafka.ProducerTopic,
+			Key: sarama.StringEncoder(
+				strconv.Itoa(int(msgs[i].AssetID)),
+			),
+			Value: sarama.ByteEncoder(data),
+		}
+	}
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ main.go
@@ -0,0 +1,137 @@
+/*-
+ * Copyright © 2017, Jörg Pernfuß <code.jpe@gmail.com>
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package main // import "github.com/mjolnir42/twister"
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/mjolnir42/erebos"
+	"github.com/mjolnir42/legacy"
+	"github.com/mjolnir42/twister/lib/twister"
+	"github.com/wvanbergen/kafka/consumergroup"
+	kazoo "github.com/wvanbergen/kazoo-go"
+)
+
+func main() {
+	twConf := erebos.Config{}
+	if err := twConf.FromFile(`twister.conf`); err != nil {
+		log.Fatalln(err)
+	}
+
+	kfkConf := consumergroup.NewConfig()
+	kfkConf.Offsets.Initial = sarama.OffsetNewest
+	kfkConf.Offsets.ProcessingTimeout = 10 * time.Second
+	kfkConf.Offsets.CommitInterval = time.Duration(twConf.Zookeeper.CommitInterval) * time.Millisecond
+	kfkConf.Offsets.ResetOffsets = twConf.Zookeeper.ResetOffset
+
+	var zkNodes []string
+	zkNodes, kfkConf.Zookeeper.Chroot = kazoo.ParseConnectionString(twConf.Zookeeper.Connect)
+
+	consumerTopic := strings.Split(twConf.Kafka.ConsumerTopics, `,`)
+	consumer, err := consumergroup.JoinConsumerGroup(
+		twConf.Kafka.ConsumerGroup,
+		consumerTopic,
+		zkNodes,
+		kfkConf,
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// this channel is closed by the handler on error
+	handlerDeath := make(chan struct{})
+
+	offsets := make(map[string]map[int32]int64)
+	handlers := make(map[int]twister.Twister)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		h := twister.Twister{
+			Num:      i,
+			Input:    make(chan []byte, twConf.Twister.HandlerQueueLength),
+			Shutdown: make(chan struct{}),
+			Death:    handlerDeath,
+			Config:   &twConf,
+		}
+		handlers[i] = h
+		go h.Start()
+	}
+
+	fault := false
+	heartbeat := time.Tick(1 * time.Second)
+
+runloop:
+	for {
+		select {
+		case <-c:
+			for i := 0; i < runtime.NumCPU(); i++ {
+				close(handlers[i].Shutdown)
+			}
+			break runloop
+		case <-handlerDeath:
+			for i := 0; i < runtime.NumCPU(); i++ {
+				close(handlers[i].Shutdown)
+			}
+			break runloop
+		case <-heartbeat:
+			continue runloop
+		case e := <-consumer.Errors():
+			log.Println(e)
+			fault = true
+			break runloop
+		case msg := <-consumer.Messages():
+			if offsets[msg.Topic] == nil {
+				offsets[msg.Topic] = make(map[int32]int64)
+			}
+
+			if offsets[msg.Topic][msg.Partition] != 0 &&
+				offsets[msg.Topic][msg.Partition] != msg.Offset-1 {
+				// incorrect offset
+				log.Printf("Unexpected offset on %s:%d. "+
+					"Expected %d, found %d.\n",
+					msg.Topic,
+					msg.Partition,
+					offsets[msg.Topic][msg.Partition]+1,
+					msg.Offset,
+				)
+			}
+
+			// send all messages from the same host to the same handler
+			// to keep the ordering intact
+			hostID, err := legacy.PeekHostID(msg.Value)
+			if err != nil {
+				log.Println(err)
+				fault = true
+				break runloop
+			}
+			handlers[hostID%runtime.NumCPU()].Input <- msg.Value
+
+			offsets[msg.Topic][msg.Partition] = msg.Offset
+			consumer.CommitUpto(msg)
+		}
+	}
+	if err := consumer.Close(); err != nil {
+		log.Println(`Error closing the consumer:`, err)
+	}
+	if fault {
+		// let the service supervisor know the shutdown was not
+		// planned
+		os.Exit(1)
+	}
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
