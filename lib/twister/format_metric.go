1a46172a4599e57ea02d7378d4baeb18b74803d8
Jörg Pernfuß <code.jpe@gmail.com>
2017-06-16 21:16:24 +0000

    Implement legacy.Formatter function

---
+++ lib/twister/format_metric.go
@@ -0,0 +1,35 @@
+/*-
+ * Copyright © 2017, Jörg Pernfuß <code.jpe@gmail.com>
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package twister // import "github.com/mjolnir42/twister/lib/twister"
+import (
+	"fmt"
+
+	"github.com/mjolnir42/legacy"
+	metrics "github.com/rcrowley/go-metrics"
+)
+
+// FormatMetrics is the formatting function to export Mistral metrics
+// via legacy.MetricSocket, implementing legacy.Formatter
+func FormatMetrics(batch *legacy.PluginMetricBatch) func(string, interface{}) {
+	return func(metric string, v interface{}) {
+		switch v.(type) {
+		case *metrics.StandardMeter:
+			value := v.(*metrics.StandardMeter)
+			batch.Metrics = append(batch.Metrics, legacy.PluginMetric{
+				Type:   `float`,
+				Metric: fmt.Sprintf("%s/avg/rate/1min", metric),
+				Value: legacy.MetricValue{
+					FlpVal: value.Rate1(),
+				},
+			})
+		}
+	}
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
